88b3d11f41f5ed923250f3059585bcf5d3103e82
chongchong0117 <2078482415@qq.com>
2024-11-24 22:31:49 +0800

    Update clock.js

--- clock.js
+++ clock.js
@@ -32,10 +32,9 @@ const data = {
     // 格式化时间为完整的时间字符串，精确到毫秒并包含时区
     let timeString = `${this.year}年${this.month < 10 ? '0' + this.month : this.month}月${this.day < 10 ? '0' + this.day : this.day}日 ` +
            `${this.weekday} ` +
-           `${this.hours < 10 ? '0' + this.hours : this.hours}:${this.minutes < 10 ? '0' + this.minutes : this.minutes}:${this.seconds < 10 ? '0' + this.seconds : this.seconds}.${this.milliseconds < 100 ? '0' + (this.milliseconds < 10 ? '0' + this.milliseconds : this.milliseconds) : this.milliseconds} ` +
-           `时区: ${this.timeZoneOffset}`;
+           `${this.hours < 10 ? '0' + this.hours : this.hours}:${this.minutes < 10 ? '0' + this.minutes : this.minutes}:${this.seconds < 10 ? '0' + this.seconds : this.seconds}} `;
 
-    // 如果分钟数为50，输出“还差十秒一分钟”
+    // 如果分钟数为20或50
     if (this.seconds===20||this.seconds === 50) {
       timeString += "\n站起来活动一下吧！";
     }
